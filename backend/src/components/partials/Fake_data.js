f8d13a2d4a1b2e345fe2e8389a767c34c9c78a78
khaprotein633 <merikavo2@gmail.com>
2024-10-14 13:44:14 +0700

    aa

--- backend/src/components/partials/Fake_data.js
+++ backend/src/components/partials/Fake_data.js
@@ -2,13 +2,13 @@ export const linerChartData = {
     labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' , 'Sunday'],
     datasets: [
       {
-        label: 'Sục cặc trong tuần',
+        label: 'aa',
         data: [2, 2, 2, 3, 1, 6, 0,0],
         borderColor: 'rgb(75, 192, 192)',
       
       },
       {
-        label: 'Chơi game trong tuần',
+        label: 'bb',
         data: [3, 2, 9, 10, 0, 20, 15],
         borderColor: 'rgb(102, 19, 19)',
       
